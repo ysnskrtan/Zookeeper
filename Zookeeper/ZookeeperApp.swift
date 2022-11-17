dec6ff1726bb6cd3ab4d15ff1f6ea26c182dcc16
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-17 12:57:51 +0300

    Zookeper project created

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Yasin Şükrü Tan on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
